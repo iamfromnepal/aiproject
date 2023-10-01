16b19619be193eae06ae6dd4fcdb04e2425112fa
Sumit Gurung <ss.dev.n118@gmail.com>
2023-10-01 23:40:20 +0545

    Section added

--- js/main.js
+++ js/main.js
@@ -17,4 +17,83 @@ $(document).ready(function () {
 
 		$(this).toggleClass('open');
 	});
+
+	// footer toggle
+	$(".toggleBtn").click(function () {
+		$(".toggleBtn")
+			.not(this)
+			.siblings(".toggleBlock")
+			.slideUp();
+		$(this).toggleClass("active");
+		$(".toggleBtn").not(this).removeClass("active");
+		$(this).siblings(".toggleBlock").slideToggle();
+	});
+
+	// accordion toggle
+	$('.accordion').click(function (e) {
+		e.stopPropagation();
+		if ($(e.target).hasClass('accordionBtn') && $(e.target).hasClass('active')) {
+			$(e.target).removeClass('active');
+			$(e.target).siblings('.accordionBody').slideUp();
+			$(e.target).parent('.accordion-item').removeClass('itemActive');
+		} else if ($(e.target).hasClass('accordionBtn')) {
+			$('.accordionBtn').removeClass('active');
+			$(e.target).toggleClass('active');
+			$('.accordionBtn').siblings('.accordionBody').slideUp();
+			$(e.target).siblings('.accordionBody').slideToggle();
+			$('.accordionBtn').parent('.accordion-item').removeClass('itemActive');
+			$(e.target).parent('.accordion-item').toggleClass('itemActive');
+		}
+	});
+
+	//tabs
+	$('ul.tabs li .tab-link').click(function (e) {
+		e.preventDefault();
+		var $this = $(this);
+		var $theTab = $(this).attr('id');
+		console.log($theTab);
+		if ($this.hasClass('active')) {
+			// do nothing
+		} else {
+			$this.closest('.tab-block').find('ul.tabs li .tab-link, .tab-item .tabContent').removeClass('active');
+			$('.tab-item .tabContent[data-tab="' + $theTab + '"], ul.tabs li .tab-link[id="' + $theTab + '"]').addClass('active');
+		}
+		// $('.default-slider').slick('refresh');
+	});
+
+	//toggleAccodrion
+	$(".toggleTabBtn").click(function () {
+		$(".toggleTabBtn")
+			.not(this)
+			.siblings(".toggleTabBlock")
+			.slideUp();
+		$(this).toggleClass("active");
+		$(".toggleTabBtn").not(this).removeClass("active");
+		$(this).siblings(".toggleTabBlock").slideToggle();
+	});
+
+	// $('.toggleTabBtn').click(function () {
+	// 	$('.default-slider').slick('refresh');
+	// });
+
+	// slider-col-3
+	var swiper = new Swiper(".slider-col-3", {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		// Responsive breakpoints
+		breakpoints: {
+			576: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 20
+			}
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 });
