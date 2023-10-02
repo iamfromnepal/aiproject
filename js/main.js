226cba7045c3a7a812cce91bc7d969b8abfa82c6
Sudhan <iamfromnepal94@gmail.com>
2023-10-02 20:26:08 +0545

    Fixes

--- js/main.js
+++ js/main.js
@@ -83,10 +83,14 @@ $(document).ready(function () {
 		// Responsive breakpoints
 		breakpoints: {
 			576: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			768: {
 				slidesPerView: 2,
 				spaceBetween: 20
 			},
-			1024: {
+			1200: {
 				slidesPerView: 3,
 				spaceBetween: 20
 			}
